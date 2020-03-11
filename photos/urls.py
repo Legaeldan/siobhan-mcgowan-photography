701c857e070804043698d0a9fac375cb0ee2c739
Legaeldan <legaeldan@gmail.com>
2020-03-11 15:29:40 +0000

    Updated URLs for photo details view.

--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import all_photos
+from .views import all_photos, photo_detail
 
 urlpatterns = [
     url(r'^$', all_photos, name='photos'),
+    url(r'^(?P<pk>\d+)/$', photo_detail, name='photo_detail')
 ]
