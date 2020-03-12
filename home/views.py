b2ca1f331309c11e54ae5435bcf0a9f46952c16b
Legaeldan <legaeldan@gmail.com>
2020-03-12 02:52:19 +0000

    Added all photos to home view.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from photos.models import Photo
 
 # Create your views here.
 def index(request):
     """A view that displays the index page"""
-    return render(request, "index.html")
+    photos = Photo.objects.all()
+    return render(request, "index.html", {"photos": photos})
