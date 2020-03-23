85183ce530d75fb45571966a663402f4edb11f4f
Legaeldan <legaeldan@gmail.com>
2020-03-23 18:34:34 +0000

    Allowed preview image to be blank.

--- photos/models.py
+++ photos/models.py
@@ -20,7 +20,7 @@ class Photo(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     published_date = models.DateTimeField(blank=True, default=timezone.now)
     image = models.ImageField(upload_to='images')
-    previewimage = models.ImageField(upload_to='images/preview')
+    previewimage = models.ImageField(upload_to='images/preview', blank=True)
     featured = models.BooleanField(default=False)
     active = models.BooleanField(default=True)
 
