96b2ab49dadefa1d4cd98d4b916c35ec3911fc57
Legaeldan <legaeldan@gmail.com>
2020-03-23 18:34:01 +0000

    Added company to review model.

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class Review(models.Model):
     name = models.CharField(max_length=60)
+    company = models.CharField(max_length=60, blank=True)
     description = models.TextField(max_length=180)
     timestamp = models.DateTimeField(blank=True, default=timezone.now)
     featured = models.BooleanField(default=False)
