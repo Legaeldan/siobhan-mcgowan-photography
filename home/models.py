1714bb0ab838af1ba5125beabe3d9f6cbbce7486
Legaeldan <legaeldan@gmail.com>
2020-03-23 17:36:40 +0000

    Added model for reviews on home.

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Review(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(max_length=180)
+    timestamp = models.DateTimeField(blank=True, default=timezone.now)
+    featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
