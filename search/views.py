f29897b718a28fe45ca87d31c04994f540cfc771
Legaeldan <legaeldan@gmail.com>
2020-03-23 19:53:41 +0000

    Updated search query from complex queries.

--- search/views.py
+++ search/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from photos.models import Photo
+from django.db.models import Q
 
 # Create your views here.
 def do_search(request):
-    photos = Photo.objects.filter(name__icontains=request.GET['q'])
+    photos = Photo.objects.filter(Q(name__icontains=request.GET['q']) | Q(category__name__icontains=request.GET['q']))
     return render(request, "photos.html", {"photos": photos})
