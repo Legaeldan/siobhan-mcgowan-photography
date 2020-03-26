e218aa879288b862de6ffa850fcbb63006b120c1
Legaeldan <legaeldan@gmail.com>
2020-03-26 14:21:27 +0000

    updated search to allow for tags.

--- search/views.py
+++ search/views.py
@@ -4,5 +4,5 @@ from django.db.models import Q
 
 # Create your views here.
 def do_search(request):
-    photos = Photo.objects.filter(Q(name__icontains=request.GET['q']) | Q(category__name__icontains=request.GET['q']))
+    photos = Photo.objects.filter(Q(name__icontains=request.GET['q']) | Q(category__name__icontains=request.GET['q']) | Q(tags__icontains=request.GET['q']))
     return render(request, "photos.html", {"photos": photos})
